a89c1b70321544730f01908d82a99a0b5f2cb574
yes-nt <70011254+yes-nt@users.noreply.github.com>
2020-09-05 21:33:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var gun,wall
+
+var speed,weight
+
+var thickness
+
+
+function setup() {
+createCanvas(800,400);
+
+thickness = random(22,83);
+
+wall= createSprite(800, 20,thickness,800);
+
+gun = createSprite(100,200,50,10);
+
+
+
+speed = random(223,321);
+
+weight = random(30,52,1500);
+}
+
+function draw() {
+  background(0,0,0);
+  gun.velocityX = speed/4;
+  if(wall.x-gun.x < gun.width+wall.width/2)
+  {
+   gun.velocityX = 0;
+  
+var deformation= (0.5 * weight* speed* speed)/(thickness*thickness*thickness);
+  if(deformation<40){
+  wall.shapeColor=color("green");
+  stroke("white");
+  textSize(20);
+  fill ("white");
+  text ("That's a really safe wall! :D",100,100);  
+  }
+  if(deformation>40 ){
+  wall.shapeColor=color("red");
+  stroke("white");
+  textSize(20);
+  fill ("white");
+  text ("The wall is too unsafe to use. D:",100,100);  
+  }
+  }
+
+  drawSprites();
+}
